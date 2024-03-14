596bb19d0fffe4123c1411f191bb263b6c3b647c
5756709@stu.kmu.ac.kr <5756709@stu.kmu.ac.kr>
2024-03-14 11:56:38 +0900

    소수의 합 clock

--- 5756709/0312_5756709.c
+++ 5756709/0312_5756709.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <time.h>
 
 int main()
 {
 	int a = 0;
+	long int start, stop = 0;
 	int p = 0;
 
+	start = clock(); //측정 시작
 	for (int n = 2; n <= 100; n++)
 	{
 		for (int i = 2; i < n; i++)
@@ -21,8 +24,12 @@ int main()
 		}
 
 		a = 0;
-	}
-	printf("%d\n", p);
+	}  //소수의 합 구하는 루프
+	stop = clock(); //측정 종료
+	printf("%d\n", p); //소수의 합 출력
 
+	double duration = (double)(stop - start) / CLOCKS_PER_SEC; //시간 계산
+
+	printf("%f", duration); //시간 출력
 	return 0;
 }
