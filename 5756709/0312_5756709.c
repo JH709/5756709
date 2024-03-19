e3f301c44fd4123864db4ba0d2f0ad9dac1ff595
5756709@stu.kmu.ac.kr <5756709@stu.kmu.ac.kr>
2024-03-19 09:19:46 +0900

    0319

--- 5756709/0312_5756709.c
+++ 5756709/0312_5756709.c
@@ -2,27 +2,6 @@
 
 int main()
 {
-	int a = 0;
-	int p = 0;
-
-	for (int n = 2; n <= 100; n++)
-	{
-		for (int i = 2; i < n; i++)
-		{
-			if (n % i == 0) {
-				a = 1;
-				break;
-			}
-		}
-
-		if (a == 0)
-		{
-			p = p + n;
-		}
-
-		a = 0;
-	}
-	printf("%d\n", p);
-
+	
 	return 0;
 }
